25e4ad12a701c04cbfec70b175dc48f10c78a3f9
takashi.h <1samuraiboy@gmail.com>
2020-07-13 23:23:40 +0900

    first commit

---
+++ increase-1gb-auditlog.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+## Log Function
+readonly LOGFILE="/var/log/jamf-cis/${0##*/}.log"
+readonly PROCNAME=${0##*/}
+function log() {
+  local fname=${BASH_SOURCE[1]##*/}
+  /bin/echo "$(date '+%Y-%m-%dT%H:%M:%S') ${PROCNAME} (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $@" | tee -a ${LOGFILE}
+}
+  mkdir -p /var/log/jamf-cis/
+  touch ${LOGFILE}
+
+## Main
+Check_Flag=`cat /etc/security/audit_control | egrep expire-after`
+CMD=`sed -i '' -e "s/expire-after:10M/expire-after:60D OR 1G/g" /etc/security/audit_control`
+
+if [ ${Check_Flag} = 'expire-after:60D OR 1G' ]
+then
+	log "audit_control acl check"
+	log "/etc/security/audit_control is OK"
+else
+	log "audit_control acl check"
+	log "/etc/security/audit_control is NG"
+	log "Audit Log increase 60D OR 1G"
+	${CMD}
+fi
